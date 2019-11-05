5a6c583c7b5bcf96419cc71908cf2d3a2552528b
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2019-11-05 20:59:08 -0300

    Changed redirect method

--- src/com/maxprograms/mvdserver/RedirectHandler.java
+++ src/com/maxprograms/mvdserver/RedirectHandler.java
@@ -34,32 +34,14 @@ public class RedirectHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         URI uri = exchange.getRequestURI();
-        
+
         String newLocation = "https://" + parent.getHostName() + uri.toString();
-        
+
         logger.log(Level.INFO, "redirecting to " + newLocation);
 
-        StringBuilder builder = new StringBuilder();
-        builder.append("<html><head><meta http-equiv=\"refresh\" content=\"0; URL=");
-        builder.append(newLocation);
-        builder.append("\"/><title>&nbsp;</title></head>&nbsp;<body></body></html>");
-
-        exchange.getResponseHeaders().add("content-type", "text/html");
-        exchange.getResponseHeaders().add("X-FRAME-OPTIONS", "sameorigin");
-        exchange.getResponseHeaders().add("X-XSS-Protection", "1; mode=block");
-        exchange.getResponseHeaders().add("X-Content-Type-Options", "nosniff");
-        exchange.getResponseHeaders().add("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
-        exchange.getResponseHeaders().add("X-Permitted-Cross-Domain-Policies", "master-only");
-        exchange.getResponseHeaders().add("Content-Security-Policy", "report-uri https://maxprograms.com");
-        exchange.getResponseHeaders().add("Referrer-Policy", "no-referrer-when-downgrade");
-        exchange.getResponseHeaders().add("Feature-Policy", "microphone 'none'; camera 'none'");
-
-        byte[] array = builder.toString().getBytes(StandardCharsets.UTF_8);
-
-        exchange.sendResponseHeaders(200, array.length);
-        try (OutputStream os = exchange.getResponseBody()) {
-            os.write(array, 0, array.length);
-        }
+        exchange.getResponseHeaders().add("Upgrade-Insecure-Requests", "1");
+        exchange.getResponseHeaders().add("Location", newLocation);
+        exchange.sendResponseHeaders(307, -1);
     }
 
 }
