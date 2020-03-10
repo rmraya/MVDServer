6752293ffdaa90551971b78244377cab83b4994d
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2020-03-10 10:15:27 -0300

    Added content types for fonts

--- src/com/maxprograms/mvdserver/Constants.java
+++ src/com/maxprograms/mvdserver/Constants.java
@@ -17,6 +17,6 @@ public class Constants {
         // do not instantiate this class
     }
 
-    public static final String VERSION = "1.2.01";
-    public static final String BUILD = "20200207_2050";
+    public static final String VERSION = "1.2.2";
+    public static final String BUILD = "20200310_1014";
 }
